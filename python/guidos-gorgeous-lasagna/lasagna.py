fcc322c0aeb82d6c104c4447eb55d3ab1839f136
GenaNasosov <genanasosovlive@gmail.com>
2022-01-29 18:39:37 +0300

    Lab2

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -1,10 +1,9 @@
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
+"""Lab2"""
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
 
 
-# TODO: define the 'bake_time_remaining()' function
-def bake_time_remaining():
+def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int baking time already elapsed.
@@ -14,10 +13,21 @@ def bake_time_remaining():
     an argument and returns how many minutes the lasagna still needs to bake
     based on the `EXPECTED_BAKE_TIME`.
     """
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-    pass
+def preparation_time_in_minutes(number_of_layers):
+    """Return preparation time in minutes.
+    
+    Function that takes the number of layers you want to add to the lasagna 
+    as an argument and returns how many minutes you would spend making them. 
+    Assume each layer takes 2 minutes to prepare.
+    """
+    return number_of_layers * PREPARATION_TIME
 
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Return elapsed cooking time.
 
-# TODO: define the 'elapsed_time_in_minutes()' function
+    This function takes two numbers representing the number of layers & the time already spent 
+    baking and calculates the total elapsed minutes spent cooking the lasagna.
+    """
+    return elapsed_bake_time + preparation_time_in_minutes(number_of_layers)
---
+++ python/guidos-gorgeous-lasagna/test.py
@@ -0,0 +1,16 @@
+from operator import ne
+
+
+def new(one, two):
+    result = one + two
+    return result
+
+a = new(4, 7)
+
+a = 8
+
+a = "Hello"
+
+b = new("hello ", "world")
+
+print(b)
